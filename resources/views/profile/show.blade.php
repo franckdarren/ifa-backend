bdda1e68b050930ff296bf4c0b0e903a04cd3588
Franck Darren <kikondanze@gmail.com>
2025-04-06 00:11:36 +0100

    fix

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight mt-4 md:mt-0">
             {{ __('Profile') }}
         </h2>
     </x-slot>
