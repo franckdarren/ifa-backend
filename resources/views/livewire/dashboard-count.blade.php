4a4e25a4408e50f98a329ab59e6ff42a9c22247c
Franck Darren <kikondanze@gmail.com>
2025-04-06 00:54:24 +0100

    fix

--- resources/views/livewire/dashboard-count.blade.php
+++ resources/views/livewire/dashboard-count.blade.php
@@ -2,32 +2,32 @@
     <div
         class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 text-center">
         <h5 class="mb-2 text-5xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $nbreClient }}</h5>
-        <p class="font-normal text-gray-700 dark:text-white">Nombre de clients</p>
+        <p class="font-normal text-gray-700 dark:text-white">Client(s)</p>
     </div>
     <div
         class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 text-center">
         <h5 class="mb-2 text-5xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $nbreBoutique }}
         </h5>
-        <p class="font-normal text-gray-700 dark:text-white">Nombre de boutiques</p>
+        <p class="font-normal text-gray-700 dark:text-white">Boutique(s)</p>
     </div>
     <div
         class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 text-center">
         <h5 class="mb-2 text-5xl font-bold tracking-tight text-gray-900 dark:text-white">
             {{ $nbreLivraison }}
         </h5>
-        <p class="font-normal text-gray-700 dark:text-white">Nombre de livraisons</p>
+        <p class="font-normal text-gray-700 dark:text-white">Livraison(s)</p>
     </div>
     <div
         class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 text-center">
         <h5 class="mb-2 text-5xl font-bold tracking-tight text-gray-900 dark:text-white">
             {{ $nbreArticle }}
         </h5>
-        <p class="font-normal text-gray-700 dark:text-white">Nombre d'articles</p>
+        <p class="font-normal text-gray-700 dark:text-white">Article(s)</p>
     </div>
     <div
         class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 text-center">
         <h5 class="mb-2 text-5xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $nbreCommande }}
         </h5>
-        <p class="font-normal text-gray-700 dark:text-white ">Nombre de commandes</p>
+        <p class="font-normal text-gray-700 dark:text-white ">Commande(s)</p>
     </div>
 </div>
